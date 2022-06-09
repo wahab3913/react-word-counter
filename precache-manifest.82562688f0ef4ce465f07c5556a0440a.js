04e13dc901e0e6abdbb43b3ed04b48877a712077
wahab3913 <wahabnoor315@gmail.com>
2022-06-09 20:01:04 +0500

    Updates

---
+++ precache-manifest.82562688f0ef4ce465f07c5556a0440a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bc8e066b368f61cce57",
+    "url": "/react-word-counter/static/css/main.cbe25885.chunk.css"
+  },
+  {
+    "revision": "3bc8e066b368f61cce57",
+    "url": "/react-word-counter/static/js/main.3bc8e066.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/react-word-counter/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "2107fc8e9694f45e01cb",
+    "url": "/react-word-counter/static/js/2.2107fc8e.chunk.js"
+  },
+  {
+    "revision": "ed443a1a341ed2bde7a9",
+    "url": "/react-word-counter/static/js/runtime~main.ed443a1a.js"
+  },
+  {
+    "revision": "3274aa1a0a8cdc4d8cc218297ae75796",
+    "url": "/react-word-counter/index.html"
+  }
+];
